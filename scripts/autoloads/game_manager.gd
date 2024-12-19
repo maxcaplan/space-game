219556eb11418a4f4c6711bcdeb067e0c9079569
maxcaplan <maxacaplan@gmail.com>
2024-12-19 17:51:33 -0400

    Added basic player animations and blend tree

--- scripts/autoloads/game_manager.gd
+++ scripts/autoloads/game_manager.gd
@@ -1,2 +1,13 @@
 class_name GameManagerAutoload
 extends Node
+
+func _ready():
+	var scenes: Array[String] = []
+
+	for i in range(1000):
+		scenes.append("res://scenes/characters/player_character/player_character.tscn")
+
+	LoadingManager.threaded_load_scene_array(scenes).scenes_loaded.connect(_on_scenes_loaded)
+
+func _on_scenes_loaded(scenes: Array[Resource], _group):
+	print("%s scenes loaded" % scenes.size())
